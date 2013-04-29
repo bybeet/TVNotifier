768c11d512ce18767561133e3b098266f8d8f09f
bybeet <travis.bybee@gmail.com>
2013-04-28 21:44:43 -0600

    Updated User model with validation and ensure secure password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password_digest, :username
+  attr_accessible :email, :password, :password_confirmation, :username
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  has_secure_password
 end
