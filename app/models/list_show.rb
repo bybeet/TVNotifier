9af0525bf4b33dd4f01d8c8203f206811eb8cf16
bybeet <travis.bybee@gmail.com>
2013-04-28 23:02:38 -0600

    Added foreign relations to the list_show model and user model.

--- app/models/list_show.rb
+++ app/models/list_show.rb
@@ -1,3 +1,6 @@
 class ListShow < ActiveRecord::Base
+  belongs_to :show
+  belongs_to :user
+  
   attr_accessible :show_id, :user_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   has_secure_password
+
+  #Many list_shows, destory the listshow for the user when user is destroyed.
+  has_many :list_shows, dependent: :destory
 end
