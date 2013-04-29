8cafcbcff1b56b54e20419f195ec70f14ebd15c3
bybeet <travis.bybee@gmail.com>
2013-04-28 19:25:54 -0600

    Initial commit with a newly generated rails server.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TvNotifier::Application.config.secret_token = '206155e1a5c2f00fc00bb65265c5e006ace7c22b1c2e6a6ba1de5df74fa6b61c10488ff07ae4d80e012b814b02aebee77f8293331d2c3ef651969fc6dfb03483'
